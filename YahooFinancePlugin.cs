883059a84e43169da037a233522c963765565350
Dean Lunz <deanlunz@createdbyx.com>
2020-03-01 23:52:56 -0800

    Initial Commit

---
+++ YahooFinancePlugin.cs
@@ -0,0 +1,65 @@
+﻿namespace YahooFinanceADPlugin
+{
+    using System.ComponentModel.Composition;
+    using Codefarts.AutoDownloader;
+    using Codefarts.AutoDownloader.Interfaces;
+
+    [Export(typeof(ISourcePlugin))]
+    [SourcePlugin("Yahoo Finance")]
+    public class YahooFinancePlugin : ISourcePlugin
+    {
+        private bool isRunning;
+        private ApplicationModel applicationModel;
+
+        public string Title
+        {
+            get
+            {
+                return "Yahoo Finance";
+            }
+        }
+
+        public bool IsRunning
+        {
+            get
+            {
+                return this.isRunning;
+            }
+        }
+
+        public void Run()
+        {
+            if (this.isRunning)
+            {
+                return;
+            }
+
+            this.isRunning = true;
+            this.isRunning = false;
+        }
+
+        public void Stop()
+        {
+            this.isRunning = false;
+        }
+
+        public ApplicationModel Application
+        {
+            get
+            {
+                return this.applicationModel;
+            }
+        }
+
+        public void Connect(ApplicationModel appModel)
+        {
+            this.applicationModel = appModel;
+        }
+
+        public void Disconnect()
+        {
+            this.Stop();
+            this.applicationModel = null;
+        }
+    }
+}
---
+++ YahooFinancePluginView.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace YahooFinanceADPlugin
+{
+    using System.Windows.Controls;
+
+    /// <summary>
+    /// Interaction logic for YahooFinancePluginView.xaml
+    /// </summary>
+    public partial class YahooFinancePluginView : UserControl
+    {
+        public YahooFinancePluginView()
+        {
+            InitializeComponent();
+        }
+    }
+}
